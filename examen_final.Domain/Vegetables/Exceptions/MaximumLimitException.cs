a629b2a04141e4f6c4ab6b69537c86884badf5bc
Jhonatan Alejandro Muñoz Serna <jhonatan.munoz@grupoemi.com>
2024-11-30 12:23:10 -0500

    fix

--- examen_final.Domain/Vegetables/Exceptions/MaximumLimitException.cs
+++ examen_final.Domain/Vegetables/Exceptions/MaximumLimitException.cs
@@ -1,7 +1,7 @@
 ﻿namespace examen_final.Domain.Vegetables.Exceptions;
 public class MaximumLimitException : ApplicationException {
     public MaximumLimitException() 
-        : base( "Ha superado el límite máximos a adición a la suscripción" ) {
+        : base( "Ha superado el límite máximos de adición a la suscripción" ) {
 
     }
 }
